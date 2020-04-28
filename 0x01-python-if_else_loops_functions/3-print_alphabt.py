5be69a05920e1f6dde5302f75da47747016175d0
saidskander <askander199h@outlook.com>
2020-04-28 22:31:48 +0100

    Done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-x = 0
 for i in range(97, 123):
-  if i == ord("q") or i == ord("e"):
-   continue
-  print(chr(i), end="")
-# other example: print("{:c}".format(i), end="")
+    if i == ord("q") or i == ord("e")
+    continue
+    print(chr(i), end="")
+    print("{:c}".format(i), end="")

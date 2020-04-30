50f26284a8567cae8973c20dac89dc32808a31db
saidskander <askander199h@outlook.com>
2020-04-30 17:27:01 +0100

    Done

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+def main():
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+
+    main()
+
+# THIS IS ANOTHER EXAMPLE
+
+# if __name__ == "__main__":
+#    from add_0 import add
+#    a = 1
+#    b = 2
+#    print("{} + {} = {}".format(a, b, add(a, b)))

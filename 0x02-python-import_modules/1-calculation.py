1968cf51172cf857cd686fb1e5ebe3d98db3c93b
saidskander <askander199h@outlook.com>
2020-04-30 17:59:30 +0100

    Done

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import sub, add, div, mul
+
+
+def main():
+    a = 10
+    b = 5
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+
+
+if __name__ == "__main__":
+    main()
+
+# ANOTHER EXAMPLE
+
+# if __name__ == "__main__":
+#    from calculator_1 import add, sub, mul, div
+#    a = 10
+#    b = 5
+#    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+#    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+#    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+#    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))

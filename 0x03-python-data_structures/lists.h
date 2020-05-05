18fc5c91ec150d08485b9cba5a2bc7ad9f84c456
saidskander <askander199h@outlook.com>
2020-05-05 20:14:02 +0100

    Done

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,6 @@
 #ifndef list_h
 #define list_h
 
-def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
 def replace_in_list(my_list, idx, element):
 def print_reversed_list_integer(my_list=[]):

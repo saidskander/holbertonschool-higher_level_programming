275862606fd46c677a547dcc6a563f7ae03adeeb
saidskander <askander199h@outlook.com>
2020-05-05 22:09:10 +0100

    Done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     dup = my_list[:]
     if idx >= 0:
-        dup[idx] = element
+        return dup[idx] = element
     if idx < len(dup):
-        dup[idx] = element
+        return dup[idx] = element
     return dup

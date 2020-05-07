f87924317f5615f3815c87e1f614be80e2c800a5
saidskander <askander199h@outlook.com>
2020-05-07 20:55:00 +0100

    Done

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    x = 0
-    for valueKey in a_dictionary:
-        if valueKey == key:
-            a_dictionary[valueKey] = value
-            x = 1
-    if x == 0:
-        a_dictionary[key] = x
+    a_dictionary[key] = value
     return a_dictionary

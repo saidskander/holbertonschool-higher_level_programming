12171533471a6f40137f630c24defebc574fb46e
saidskander <askander199h@outlook.com>
2020-06-04 00:26:20 +0100

    Done

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """representation of an object: JSON is the best"""
 
 
+import json
 def to_json_string(my_obj):
     """json"""
     jsonresult = json.dumps(my_obj)

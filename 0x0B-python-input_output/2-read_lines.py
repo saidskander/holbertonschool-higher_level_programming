2e5d5bf707fa4e3507c37be234696466abc9100d
saidskander <askander199h@outlook.com>
2020-06-03 19:37:44 +0100

    Done

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,7 +2,7 @@
 """Read the lines of the File"""
 
 
-def read_lines(filename="", numberoflines = 0):
+def read_lines(filename="", numberoflines=0):
     """reading lines"""
     with open(filename, 'r', encoding='utf8') as File:
         n = sum(1 for line in open(filename))

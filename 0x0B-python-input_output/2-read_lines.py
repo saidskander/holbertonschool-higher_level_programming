1ecceedf9914d423041fdcf8c61643fd6707c3cd
saidskander <askander199h@outlook.com>
2020-06-03 19:34:47 +0100

    Done

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Read the lines of the File"""
+
+
+def read_lines(filename="", numberoflines = 0):
+    """reading lines"""
+    with open(filename, 'r', encoding='utf8') as File:
+        n = sum(1 for line in open(filename))
+        if numberoflines <= 0 or numberoflines > n:
+            print(File.read(), end="")
+        else:
+            for line in range(0, numberoflines):
+                print(File.readline(), end="")
+    File.closed

7bce5910800843761319778db7ab4979573d8abf
saidskander <askander199h@outlook.com>
2020-06-09 22:22:13 +0100

    Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""base import"""
+"""
+importing the Base
+module from moduls
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class rectangle that inherits from Base"""
+    """Rectangle is class rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """init"""
         super().__init__(id)
@@ -15,57 +18,17 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """self width"""
-        self.__width = widht
+        """self widht"""
+        self.__width = width
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """self setter width"""
-        if type(value) is not int:
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be > 0')
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, height):
-        if not type(height) is int:
-            raise TypeError('height must be an integer')
-        elif type(height) is int:
-            if height <= 0:
-                raise ValueError('height must be > 0')
-        elif type(height) is int and height > 0:
-            self.__height = height
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-        if not type(x) is int:
-            raise TypeError("x must be an integer")
-        elif type(x) is int:
-            if x < 0:
-                raise ValueError("x must be >= 0")
-        elif type(x) is int and x > 0:
-            self.x = x
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, y):
-        if not type(y) is int:
-            raise TypeError("y must be an integer")
-        elif type(y) is int:
-            if y < 0:
-                raise ValueError('y must be >= 0')
-        elif type(y) is int and y > 0:
-            self.__y = y
+    def width(self, width):
+        """setter widht"""
+        if not type(width) is int:
+            raise TypeError("width must be an integer")
+        elif type(width) is int:
+            if width <= 0:
+                raise ValueError("width must be > 0")
+        elif type(width) is int and width > 0:
+            self.__width = 

651134eb3953b83234a415bc9641813eab15d20c
saidskander <askander199h@outlook.com>
2020-07-14 20:50:11 +0100

    Done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
 -- Creates the MySQL server user user_0d_1.
-CREATE USER if not exist user_0d_1
-BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost';
-
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+     GRANT ALL PRIVILEGES
+        ON *.*
+        TO user_0d_1@localhost;

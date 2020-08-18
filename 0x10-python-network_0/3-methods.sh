2b50f16bade4349be446ab1cf05431843e83bc88
saidskander <askander199h@outlook.com>
2020-08-18 21:22:25 +0100

    Done

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-curl -sLI 0.0.0.0:5000/route_4 -i | grep "Allow:"| cut -d ' ' -f2
+curl -sLI 0.0.0.0:5000/route_4 -i | grep "Allow:"|cut -d ' ' -f2

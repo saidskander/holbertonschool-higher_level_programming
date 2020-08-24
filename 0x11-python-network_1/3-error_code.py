684a48332e57ee3386b9571ed6bfdcacbffce58f
saidskander <askander199h@outlook.com>
2020-08-24 23:39:44 +0100

    Done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""error handling"""
-
+"""Python script that takes in a URL,
+   sends a request to the URL and displays the body of the response
+   (decoded in utf-8"""
 
 import urllib.request
 import urllib.error

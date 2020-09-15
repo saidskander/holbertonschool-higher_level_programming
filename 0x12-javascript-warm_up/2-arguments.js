89a5716a38fecf76a777585b024378ac549aad28
saidskander <askander199h@outlook.com>
2020-09-15 20:56:52 +0100

    Done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argument = process.argv.length - 2;
+if (argument < 1) {
+  console.log('No argument');
+} else if (argument === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

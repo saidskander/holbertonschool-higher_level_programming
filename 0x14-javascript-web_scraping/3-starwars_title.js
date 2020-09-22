f9aab2897ad03c2c0d1aa3983acd528bef0ee39f
saidskander <askander199h@outlook.com>
2020-09-22 23:31:12 +0100

    Done

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 //  prints the title of a Star Wars movie where the episode number matches a given integer.
 
 const request = require('request');
-const urlrequests = `http://swapi.co/api/films/${process.argv[2]}`;
+const urlrequests = "http://swapi.co/api/vehicles/${process.argv[2]}";
 request(urlrequests, (err, response, body) => {
   if (err) {
     console.log(err);

f4d90cd6b44d1854100fd73fbb1b6e03c2f28652
saidskander <askander199h@outlook.com>
2020-09-22 23:58:31 +0100

    Done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//prints the number of movies where the character “Wedge Antilles”
+// prints the number of movies where the character “Wedge Antilles”
 const request = require('request');
 
 request(process.argv[2], function (err, res, body) {

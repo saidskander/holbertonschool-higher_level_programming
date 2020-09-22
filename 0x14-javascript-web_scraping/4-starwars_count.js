e0e88151203715e3dde6222ffc9e2704380f9b3b
saidskander <askander199h@outlook.com>
2020-09-22 23:54:09 +0100

    Done

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+//prints the number of movies where the character “Wedge Antilles”
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  let i = 0;
+  if (err) {
+    console.log(err);
+  }
+  const json = JSON.parse(body);
+  for (let x = 0; json.results[x] !== undefined; x++) {
+    if (json.results[x].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      i++;
+    }
+  }
+  console.log(i);
+});

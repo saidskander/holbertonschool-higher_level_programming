fc82bd3d35b749571d3727a5eefb54f9881c2537
saidskander <askander199h@outlook.com>
2020-12-13 19:53:19 +0200

    Done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""Display name argument of states table"""
+"""Script that lists all states from database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
-def filter_names_safe():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-        argv[4]: state name
-    """
-    if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
+if __name__ == '__main__':
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               database=sys.argv[3])
 
-        cur = db.cursor()
-
-        cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-                    ORDER BY id ASC".format(sys.argv[4]))
-        rows = cur.fetchall()
-        for i in rows:
-            print(i)
-
-        cur.close()
-        db.close()
-    else:
-        return
-
-if __name__ == "__main__":
-    filter_names_safe()
+    crsr = database.cursor()
+    crsr.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id\
+                  ASC", (sys.argv[4],))
+    row = crsr.fetchall()
+    for x in row:
+        print(x)
+    crsr.close()
+    database.close()

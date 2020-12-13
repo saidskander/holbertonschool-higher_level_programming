82f6956183661e1ee0784106d329ee16158f930e
saidskander <askander199h@outlook.com>
2020-12-13 17:21:12 +0200

    Done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that lists all states from database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def get_states():
+    """argv arguments to list from SQL_database
+       username, password, name, state name searched.
+    """
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               database=sys.argv[3])
+
+    crsr = database.cursor()
+    crsr.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                 ORDER BY id ASC".format(sys.argv[4]))
+    row = crsr.fetchall()
+    for x in row:
+        print(x)
+
+    crsr.close()
+    database.close()
+
+if __name__ == "__main__":
+    get_states()

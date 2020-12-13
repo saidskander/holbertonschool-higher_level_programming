6cf7821f543e9c91fc88fe7c5ea6daede87c6f94
saidskander <askander199h@outlook.com>
2020-12-13 19:45:13 +0200

    Done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,9 @@ import sys
 
 
 def filter_names_safe():
-    """argv arguments to list from SQL_database
-       username, password, name, state name searched.
+    """list argv from database
     """
-
-    if len(sys.argv) == 6:
+    if len(sys.argv) == 5:
         database = MySQLdb.connect(host="localhost",
                                    port=3306,
                                    user=sys.argv[1],
@@ -17,16 +15,17 @@ def filter_names_safe():
                                    database=sys.argv[3])
 
         crsr = database.cursor()
+
         crsr.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
                      ORDER BY id ASC".format(sys.argv[4]))
         row = crsr.fetchall()
         for x in row:
             print(x)
 
-       crsr.close()
-       database.close()
+        crsr.close()
+        database.close()
     else:
         return
 
 if __name__ == "__main__":
-    filter_names_safe():
+    filter_names_safe()

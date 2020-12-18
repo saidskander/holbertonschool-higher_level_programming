a37c3f5a05442f3c97008357b8b6002e667e2fc1
saidskander <askander199h@outlook.com>
2020-12-18 22:27:55 +0200

    Done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Lists all the states"""
-from sys import argv
+"""
+list all tates hbtn_0e_6_usa
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    db = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(db)
-    Session = sessionmaker(bind=db)
-    # Session
-    ses = Session()
-    # input state query
-    state_res = ses.query(State).order_by(State.id).all()
-    for item in state_res:
-        print('{}: {}'.format(item.id, item.name))
-    ses.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]
+                            ), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()

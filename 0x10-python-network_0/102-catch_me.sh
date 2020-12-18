8cc4ad34b01ca80ce48b71153d15d0c47df6751e
saidskander <askander199h@outlook.com>
2020-12-18 22:46:10 +0200

    Done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # You got me!
-curl -sL -X PUT -d "user_id=98" -H "Origin: holbertonschool" 0.0.0.0:5000/catch_me
+curl -sX PUT -d "user_id=98" -H "Origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me
